6263327b09149f70b704f5b5b4ef89690e22d0e4
dvictoria2020 <vicdelfer@gmail.com>
2021-10-13 17:22:35 -0600

    commit inicial

---
+++ index.R
@@ -0,0 +1,27 @@
+---
+title: "Felinos"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+# Carga de paquetes y de datos
+
+```{r paquetes}
+# Paquete para manipulación de datos
+library(dplyr)
+# Paquete para manejo de datos vectoriales
+library(sf)
+# Paquetes para manejo de datos raster
+library(terra)
+# Paquete para manejo de tablas
+library(DT)
+# Paquetes para graficación
+library(ggplot2)
+library(plotly)
+# Paquete para mapas interactivos
+library(leaflet)
+```
+
